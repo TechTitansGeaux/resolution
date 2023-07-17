01a707ab3b4949856fe54bfd7a1396f938b0e83c
Benjamin Klein <benjaminklein99@gmail.com>
2023-07-17 12:20:05 -0500

    changing meme on select change

--- client/components/Messages.jsx
+++ client/components/Messages.jsx
@@ -7,16 +7,19 @@ const Messages = () => {
 
   const [ bottomText, updateBottomText ] = useState('');
 
+  const [ meme, changeMeme ] = useState('Aint-Nobody-Got-Time-For-That');
+
 
 
   return (
     <div>
-      <h3>select image</h3>
-      <select name='cars' id='cars'>
-        <option value='Aint-Nobody-Got-Time-For-That'>Ain't nobody got time for that</option>
-        <option value='saab'>Saab</option>
-        <option value='opel'>Opel</option>
-        <option value='audi'>Audi</option>
+      <h3>select Meme</h3>
+      <select id='memes' onChange={(e) => { changeMeme(e.target.value); }}>
+        <option value='Aint-Nobody-Got-Time-For-That'>Ain't Nobody Got Time For That</option>
+        <option value='1990s-First-World-Problems'>1990's First World Problems</option>
+        <option value='Angry-Baby'>Angry Baby</option>
+        <option value='Confused-Granddad'>Confused Granddad</option>
+        <option value='Feels-Bad-Frog---Feels-Bad-Man'>Sad Pepe</option>
       </select>
       <h3>enter top text</h3>
       <input value={topText} onChange={(e) => { updateTopText(e.target.value); }}></input>
@@ -28,7 +31,7 @@ const Messages = () => {
       <button>send</button>
       <br></br>
       <br></br>
-      <img src={`https://apimeme.com/meme?meme=Aint-Nobody-Got-Time-For-That&top=${topText}&bottom=${bottomText}`}></img>
+      <img src={`https://apimeme.com/meme?meme=${meme}&top=${topText}&bottom=${bottomText}`}></img>
     </div>
   );
 };
