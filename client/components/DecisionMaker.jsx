79f4baa877e9557ed98bab4b2d31449d927bd6f0
skanda108 <sbthai94@gmail.com>
2023-07-17 12:04:15 -0500

    add rock paper scissors buttons to DecisionMaker.jsx

--- client/components/DecisionMaker.jsx
+++ client/components/DecisionMaker.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
+
 const DecisionMaker = () => {
+  const [hand, setHand] = useState('none');
+
   return (
-    <div>Decision Maker</div>
+    <div>
+      <h1>Decision Maker</h1>
+      <h2>You picked {hand}!</h2>
+      <button type="button"
+        onClick={() => setHand('rock')}
+      >Rock</button>
+      <button type="button"
+        onClick={() => setHand('paper')}
+      >Paper</button>
+      <button type="button"
+        onClick={() => setHand('scissors')}
+      >Scissors</button>
+    </div>
   );
 };
 
