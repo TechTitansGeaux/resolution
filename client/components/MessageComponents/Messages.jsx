751ec5ef9b30eb5b18c40a7288eb7359e10a5b33
Benjamin Klein <benjaminklein99@gmail.com>
2023-07-17 14:35:28 -0500

    able to dynamically render new views

--- client/components/MessageComponents/Messages.jsx
+++ client/components/MessageComponents/Messages.jsx
@@ -1,14 +1,21 @@
 import { React, useState } from 'react';
 
 import StartConversation from './StartConversation.jsx';
+import AllConversations from './AllConversations.jsx';
 
 const Messages = () => {
 
+  const [ view, updateView ] = useState(<AllConversations />);
+
+
   return (
     <div>
-      <StartConversation />
+      <button onClick={() => { updateView(<AllConversations />); }}>all conversations</button>
+      <button onClick={() => { updateView(<StartConversation />); }}>start conversation</button>
+      { view }
     </div>
   );
+
 };
 
 export default Messages;
--- client/components/MessageComponents/StartConversation.jsx
+++ client/components/MessageComponents/StartConversation.jsx
@@ -14,6 +14,7 @@ const StartConversation = () => {
     <div>
       <h3>select Meme</h3>
       <select id='memes' onChange={(e) => { changeMeme(e.target.value); }}>
+        <option value='Grumpy-Toad'>Grumpy Toad</option>
         <option value='Aint-Nobody-Got-Time-For-That'>Ain't Nobody Got Time For That</option>
         <option value='1990s-First-World-Problems'>1990's First World Problems</option>
         <option value='Angry-Baby'>Angry Baby</option>
@@ -22,7 +23,6 @@ const StartConversation = () => {
         <option value='Gasp-Rage-Face'>Gasp Rage Face</option>
         <option value='Frustrated-Boromir'>Frustrated Boromir</option>
         <option value='Futurama-Fry'>Futurama Fry</option>
-        <option value='Grumpy-Toad'>Grumpy Toad</option>
         <option value='Hercules-Hades'>Hercules Hades</option>
         <option value='Hillary-Clinton'>Happy Hillary Clinton</option>
         <option value='Happy-Guy-Rage-Face'>Happy Guy Crying Face</option>
