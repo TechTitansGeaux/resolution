fe57c073b362ac2637a44984c8004916296a7dc6
janvierjr <bernie@myheartsleeve.com>
2023-07-18 13:37:38 -0500

    update Navigation.jsx with collapsable menu button

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,34 +1,36 @@
 import { Outlet, NavLink } from "react-router-dom";
-// import React from "react";
+// nav collapse feature for small device widths
+import "bootstrap/js/src/collapse.js";
+
+
 
 const Navigation = () => {
   return (
     <>
       <nav className="navbar navbar-expand-md fixed-top navbar-light bg-light">
         <div className="container">
-          <NavLink className="nav-link navbar-brand" href="#" to="/">
+          <NavLink className="nav-link navbar-brand active" href="#" to="/">
             <span className="align-top">App Logo</span>
           </NavLink>
           <button
             type="button"
-            className="nav-btn navbar-toggler"
             data-bs-toggle="collapse"
-            data-bs-target="navbar-expansion"
-            aria-controls="navbar"
+            data-bs-target="#navbarToggleTarget"
+            aria-controls="navbarExpansion"
             aria-expanded="false"
             aria-label="Toggles Navigation"
+            className="navbar-toggler"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div id="navbar-expansion" className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li className="nav-item" role="navigation"></li>
+          <div className="navbar-collapse collapse" id="navbarToggleTarget">
+            <ul className="navbar-nav mr-auto mt-1 mt-lg-0">
               <li className="nav-item" role="navigation">
                 <NavLink className="nav-link" href="#" to="/Messages">
                   Messages
                 </NavLink>
               </li>
-              <li className="nav-item " role="navigation">
+              <li className="nav-item" role="navigation">
                 <NavLink className="nav-link" href="#" to="/DecisionMaker">
                   Decision Maker
                 </NavLink>
