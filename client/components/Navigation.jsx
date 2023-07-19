496455a034f6a98d6396f07e92c34ac0f2d4f267
janvierjr <bernie@myheartsleeve.com>
2023-07-19 11:45:55 -0500

    update Navigation.jsx to autoclose on NavLink click

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -2,19 +2,30 @@ import { Outlet, NavLink } from "react-router-dom";
 // nav collapse feature for small device widths
 import "bootstrap/js/src/collapse.js";
 import ResolutionLogo from "../img/resolution_app_logo_mini.svg";
+import { useState } from "react";
 
 
 
 const Navigation = () => {
+// state for navigation collapse
+  const [collapseNav, setCollapseNav ] = useState(true)
+
+// auto-close menu on click of NavLink
+  const handleCollapseNav = () => setCollapseNav(!collapseNav);
+
   return (
     <>
       <nav className="navbar navbar-expand-md fixed-top navbar-light bg-light">
         <div className="container">
-          <NavLink className="nav-link navbar-brand active" href="#" to="/Home">
+          <NavLink
+            className="nav-link navbar-brand active"
+            href="#"
+            to="/Home"
+          >
             <img
               src={ResolutionLogo}
               alt="Resolution Logo"
-              style={{width: "auto", height: "2.5em"}}
+              style={{ width: "auto", height: "2.5em" }}
             />
           </NavLink>
           <button
@@ -25,28 +36,52 @@ const Navigation = () => {
             aria-expanded="false"
             aria-label="Toggles Navigation"
             className="navbar-toggler"
+            onClick={handleCollapseNav}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="navbar-collapse collapse" id="navbarToggleTarget">
+          <div
+            className={`navbar-collapse ${collapseNav ? "collapse" : ""}`}
+            id="navbarToggleTarget"
+          >
             <ul className="navbar-nav mr-auto mt-1 mt-lg-0">
               <li className="nav-item" role="navigation">
-                <NavLink className="nav-link" href="#" to="/Messages">
+                <NavLink
+                  className="nav-link"
+                  href="#"
+                  to="/Messages"
+                  onClick={handleCollapseNav}
+                >
                   Meme Messenger
                 </NavLink>
               </li>
               <li className="nav-item" role="navigation">
-                <NavLink className="nav-link" href="#" to="/DecisionMaker">
+                <NavLink
+                  className="nav-link"
+                  href="#"
+                  to="/DecisionMaker"
+                  onClick={handleCollapseNav}
+                >
                   Decision Maker
                 </NavLink>
               </li>
               <li className="nav-item" role="navigation">
-                <NavLink className="nav-link" href="#" to="/WallOfFame">
+                <NavLink
+                  className="nav-link"
+                  href="#"
+                  to="/WallOfFame"
+                  onClick={handleCollapseNav}
+                >
                   Wall Of Fame
                 </NavLink>
               </li>
               <li className="nav-item" role="navigation">
-                <NavLink className="nav-link" href="#" to="/UserProfile">
+                <NavLink
+                  className="nav-link"
+                  href="#"
+                  to="/UserProfile"
+                  onClick={handleCollapseNav}
+                >
                   My Profile
                 </NavLink>
               </li>
